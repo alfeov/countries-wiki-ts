b2fafde9c200ef7eba5b79988b4aaf4f69bbff92
alfeov <alfeove@gmail.com>
2026-07-23 14:53:37 +0300

    initial commit, start project

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      eqeqeq: 'warn',
+      'no-else-return': 'warn',
+      'react-refresh/only-export-components': 'warn',
+      'simple-import-sort/exports': 'warn',
+      'simple-import-sort/imports': [
+        'warn',
+        {
+          groups: [
+            ['react', '^@[A-Za-z]+'], // 1. Frameworks
+            ['^@\/'], // 2. FSD
+            ['^\\.'], // 3. Relative imports
+            ['css$'], // 4. style imports
+          ],
+        },
+      ],
+    },
+  },
+])
---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import './styles/base/index.css'
+
+function App() {
+  return (
+    <>
+      <div>Hello</div>
+    </>
+  )
+}
+
+export default App
